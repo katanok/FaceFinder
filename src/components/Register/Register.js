460fb3093d67e475cc9f588df190a132a593dd95
Keith <keithkatano8@gmail.com>
2023-05-30 01:38:21 -0400

    added environment variables

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onRegister = () => {
-    fetch('http://localhost:3001/register', {
+    fetch(process.env.API_URL, {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
